1948723764c4547df43d61bafbb14c5d1f564c23
agubler <agubler@gmail.com>
2016-05-17 12:29:09 +0100

    clean up

--- bin/cli.js
+++ bin/cli.js
@@ -2,10 +2,8 @@
 "use strict";
 const program = require('commander');
 const moment = require("moment");
-
-const ContribCat = require("./lib");
-
-let config = require("./config");
+const ContribCat = require("./../lib/index");
+const config = require("./../config");
 
 program
     .version("0.0.1")
@@ -13,16 +11,15 @@ program
     .option("-d, --days <n>", "Load/Sync Days", 1)
     .parse(process.argv);
 
-var contribCat = new ContribCat(config);
-
 const mode = program.mode;
 const days = program.days;
-
 const fromDate = moment.utc().startOf("day").subtract(days, "days");
+
+const contribCat = new ContribCat(config);
 
 switch(mode) {
     case "load":
-        console.log("Load contribution data from date", fromDate.toString());
+        console.log("Load contribution data from", fromDate.toString());
         contribCat.load(fromDate)
             .then(contribCat.disconnect.bind(contribCat))
             .finally(() => {
@@ -30,7 +27,7 @@ switch(mode) {
             });
         break;
     case "sync":
-        console.log("Sync contribution data from date", fromDate.toString());
+        console.log("Sync contribution data from", fromDate.toString());
         contribCat.sync(fromDate)
             .then(contribCat.disconnect.bind(contribCat))
             .finally(() => {
@@ -38,7 +35,7 @@ switch(mode) {
             });
         break;
     default:
-        console.log("Load & sync contribution data from date", fromDate.toString());
+        console.log("Load & sync contribution data from", fromDate.toString());
         contribCat.load(fromDate)
             .then(contribCat.sync.bind(contribCat))
             .then(contribCat.disconnect.bind(contribCat))
